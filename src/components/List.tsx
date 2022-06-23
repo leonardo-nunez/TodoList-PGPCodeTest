151f2307d4c2fec56646a5bd4f0ecbf768d48b1f
Leo <lnpb@hotmail.com>
2022-06-23 09:01:21 +0200

    fix error messages

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -37,7 +37,7 @@ const List = ({
       return list;
     });
     setLists(newLists);
-  }, [todos]);
+  }, [todos]); // eslint-disable-line
 
   const moveCard = useCallback((dragIndex: number, hoverIndex: number) => {
     setTodos((prev: ITodo[]) =>
