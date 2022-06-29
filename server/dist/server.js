7edc6adfe8f4546ae9646b3b4d4948d53eb8ddd8
Leo <lnpb@hotmail.com>
2022-06-29 15:18:21 +0200

    copied server from server-old. get lists working

--- server/dist/server.js
+++ server/dist/server.js
@@ -1,16 +1,35 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const promises_1 = require("fs/promises");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('Express + TypeScript Server!');
 });
+app.get('/lists', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const data = yield (0, promises_1.readFile)('./data/lists.json');
+    const str = data.toString();
+    const json = JSON.parse(str);
+    return res.status(200).json(json);
+}));
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,36 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { readFile, writeFile } from 'fs/promises';
 
 dotenv.config();
 
+interface ITodo {
+  id: number;
+  text: string;
+  complete: boolean;
+}
+
+interface IList {
+  id: number;
+  name: string;
+  todos: ITodo[];
+}
+
 const app: Express = express();
 const port = process.env.PORT;
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server!');
+});
+
+app.get('/lists', async (_req: Request, res: Response) => {
+  const data = await readFile('./data/lists.json');
+  const str = data.toString();
+  const json = JSON.parse(str);
+  return res.status(200).json(json);
 });
 
 app.listen(port, () => {
